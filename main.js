97f90c236ba94e8894396d2c3139b19cc8d7464f
olaniyiOlawale <jao.olaniyi@gmail.com>
2022-03-24 17:23:12 +0100

    switchbtn

---
+++ main.js
@@ -0,0 +1,10 @@
+/* let check = document.getElementById("checkbox");
+
+check.addEventListener("change", myCheck);
+
+function myCheck(e){
+    document.body.classList.toggle("red");
+}
+ */
+
+checkbox.addEventListener("change", () => document.body.classList.toggle("red"))
